0865c6fec1d0fb7c8a70de8c0789609302ffedae
ShyamoliBurman <shyamoli.burman@wolterskluwer.com>
2021-09-23 11:50:18 +0530

    Made changes for the verification

--- UI_Automation/Program.cs
+++ UI_Automation/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections;
@@ -29,16 +30,41 @@ namespace UI_Automation
                 Thread.Sleep(3000);
                 driver.FindElement(By.XPath("//div[@class='s-expand-height s-include-content-margin s-latency-cf-section s-border-bottom']//img[@data-image-index='1']")).Click();
                 Thread.Sleep(3000);
-                
-
                 String windowHandle = driver.WindowHandles[1];
                 driver.SwitchTo().Window(windowHandle);
+
+                String telName=driver.FindElement(By.Id("productTitle")).Text;
+                Console.WriteLine("Element to be added in the cart:" +telName);
+
                 driver.FindElement(By.XPath("//input[@value='Add to Cart']")).Click();
                 Thread.Sleep(3000);
-                driver.FindElement(By.XPath("attach-close_sideSheet-link")).Click();
-                Thread.Sleep(3000);
+                //driver.FindElement(By.Id("attach-close_sideSheet-link")).Click();
+                //Thread.Sleep(3000);
                 driver.FindElement(By.Id("nav-cart-count-container")).Click();
                 Thread.Sleep(3000);
+
+                //IList<IWebElement> list = driver.FindElements(By.XPath("//span[@class='a-truncate-cut']"));
+
+                IList<IWebElement> list = driver.FindElements(By.XPath("//span[@class='a-truncate-cut']"));
+
+                foreach(IWebElement el in list)
+                {
+                    Console.WriteLine("In the cart:" + el.Text);
+
+                    if (el.Text.Contains(telName))
+                    {
+                        Assert.AreEqual(telName, el.Text);
+                        Console.WriteLine("Verified item in the cart is:" + el.Text);
+                    }
+                    else
+                    {
+                        Assert.AreNotEqual(telName, el.Text);
+                    }
+                }
+                
+                
+
+                
             }
             catch (Exception e)
             {
